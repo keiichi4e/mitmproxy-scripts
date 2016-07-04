f7a4ebc552c7b6e5cbef6a251adcd72873470930
Keiichi Inoue <keiichi@toreta.in>
2016-07-04 14:32:22 +0900

    Status code should have been integer

--- tweak_status_code.py
+++ tweak_status_code.py
@@ -1,6 +1,6 @@
 def start(context, argv):
     if len(argv) != 4:
-        raise ValueError('Usage: mitmproxy|mitmdump -s "tweak_status_code.py PUT /v1/something 500')
+        raise ValueError('Usage: mitmproxy|mitmdump -s "tweak_status_code.py PUT /v1/something 500"')
     context.method = argv[1]
     context.api = argv[2]
     context.status_code = argv[3]
@@ -9,4 +9,4 @@ def response(context, flow):
     req = flow.request
     res = flow.response
     if req.method == context.method and req.path.count(context.api):
-        res.status_code = context.status_code
+        res.status_code = int(context.status_code)
